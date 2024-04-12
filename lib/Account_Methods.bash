ac0e7836d4529f67320de36a4ec82d479972d86d
LucasYata <sgh4nl5forfocus@gmail.com>
2024-04-12 14:03:33 -0300

    Reworked/fixed 'Read_Account_Share' Account method
    Changes:
    * Now the method is a true relative method
    Before the method took an argument which was stated as the name of an Account, but it was treated as the path to it. Making it very tricky.
    Now the argument is stated as an Account name and is treated as such. Making it work as expected.
    * Now the method is categorized as relative
    Because it is (truly) relative now, it is categorized as such.
    * Now the method checks the Account file is an actual Account file

--- lib/Account_Methods.bash
+++ lib/Account_Methods.bash
@@ -12,19 +12,6 @@ File_Is_Account() { # Abstract Synopsis: File_Is_Account PresuntAccountFilePath 
 		return 0
 }
 
-Read_Account_Share() { # Read_Account_Share MyAccountName # returns: 0 if ok, 1 if file was not found and 2 if the value was not an integer # echoes: Integer percentage of money associated to the account
-		declare -r Account_Name="$1"
-		declare -i Percentage
-
-		if [[ ! -f "$Account_Name" ]] ; then return 1 ; fi
-		# input integer check
-		Percentage=$( grep --color=never 'Share=' "$Account_Name" | cut --field=2 --delim='=' )
-
-		if [[ ! "$Percentage" =~ ^[0-9]+$ ]] ; then return 2 ; fi
-		echo $Percentage
-		return 0
-}
-
 # Relative methods
 ## These are functions that operate over a given name. Which is expected to correspond to a file under the root dir of the program.
 Read_Account() { # Read_Account MyFooAccountName # echoes: Foundings of the Account(integer value) or nothing if an error occurred # returns: 0 if ok, 1 if other than 1 argument was passed, 2 if the account has a non integer value(including if the file is empty)
@@ -41,6 +28,7 @@ Read_Account() { # Read_Account MyFooAccountName # echoes: Foundings of the Acco
         }
 
         if [[ ! -f ~/moneybook/"$Account_Name" ]] ; then return 2 ; fi
+		
 		if ! File_Is_Account "$Account_Name" ; then return 3 ; fi
         if [[ ! "$( Get_Foundings "$Account_Name" )" =~ ^[0-9]+$ ]] ; then return 4 ; fi
 
@@ -61,4 +49,14 @@ Write_Account() { # Write_Account MyFooAccount MyNewIntegerValueFoundings # retu
 		return 0
 }
 
+Read_Account_Share() { # Read_Account_Share MyAccountName # returns: 0 if ok, 1 if file didn't make it as an moneybook Account file and 2 if the value was not an integer # echoes: Integer percentage of money associated to the Account named as specified under the root dor of moneybook
+		declare -r Account_Name="$1"
+		declare -r Account_File_Path=~/moneybook/"${Account_Name}"
+		declare -i Percentage
 
+		if ! File_Is_Account "$Account_File_Path" ; then return 1 ; fi
+		Percentage=$( grep --color=never 'Share=' "$Account_File_Path" | cut --field=2 --delim='=' )
+		if [[ ! "$Percentage" =~ ^[0-9]+$ ]] ; then return 2 ; fi
+		echo $Percentage
+		return 0
+}
