7bbd6d4db145ce4d8bb051637e7564797f924305
LucasYata <sgh4nl5forfocus@gmail.com>
2024-04-12 21:05:43 -0300

    Fix for erroneuos message on wrong second argument for separation
    The second argument for separation is the Fixed Account you want to separate money for.
    But when you pass the Fixed Account name as the second argument, the error message says the mode expects an integer as the secobd argument.

--- moneybook.bash
+++ moneybook.bash
@@ -344,7 +344,7 @@ then
 		if [[ -z "${1:+IsSet}" ]] ; then echo "Couldn't separate the money, missing arguments: Amount of money and Fixed Expense." > /dev/stderr ; return 2 ; fi
 		if [[ -z "${2:+IsSet}" ]] ; then echo "Couldn't separate the money, missing argument: Fixed Account." > /dev/stderr ; return 3 ; fi
 		if [[ ${3+IsSet} = 'IsSet' ]] ; then echo "Couldn't separate the money, exceeding arguments were passed. Aborting just in case." > /dev/stderr ; return 4 ; fi
-		if [[ ! "$1" =~ [0-9]+$ ]] ; then echo "Couldn't parse the second argument as a positive integer." > /dev/stderr ; return 5 ; fi
+		if [[ ! "$1" =~ [0-9]+$ ]] ; then echo "Couldn't parse the first argument as a positive integer." > /dev/stderr ; return 5 ; fi
 
 		declare -i Income="$1"
 		declare Expense_Name="$2"
