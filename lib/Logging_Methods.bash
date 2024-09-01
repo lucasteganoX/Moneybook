6a17c6ab245a21968da6000fba14cb6d1633ad02
LucasYata <sgh4nl5forfocus@gmail.com>
2024-08-31 22:17:09 -0300

    Abstracted the Log File checks from the Logging itself
    I was thinking... My plan was "Now it will try to log the purchase. And if it can't it will warn the user."
    But it would be very annoying for it to alter the funds regardless anyway and push you to manually fix it. But also...
    
    I would like to warn the user if the logging can't be done, before commiting the purchase. For example, if there's no log file, I would like the user to be informed before commiting the purchse instead of it failing after trying. It could fail after waiting for the user to accept if something changed, sure, but it's different that checking before trying.
    
    So I will start out by preparing the later. And for the other problem, I will surely try to log first, and if it can be done, then I will ask the user if they want to continue anyway.

--- lib/Logging_Methods.bash
+++ lib/Logging_Methods.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-Log_Purchase() { # synopsis: Log_Purchase Account_Name Purchase_Cost Purchase_Message # note: write a purchase entry to the log file. It needs the name of the account the purchase was made over, its cost and optionally a message to describe the purchase 
+Logging_Is_Possible() { # synopsis: Logging_Is_Possible # note: It checks everything is set in order to log something. # returns: 0 if everything is fine, 1 if the file doesn't exist, 2 if it is not a regular file, 3 if it's not writable.
+		declare -r Log_Directory_Path='/data/data/com.termux/files/usr/var/log'
+		declare -r Log_File_Path="${Log_Directory_Path}/moneybook.log"
 
+		if [[ ! -a "$Log_File_Path" ]] ; then return 1 ; fi
+		if [[ ! -f "$Log_File_Path" ]] ; then return 2 ; fi
+		if [[ ! -w "$Log_File_Path" ]] ; then return 3 ; fi
+		return 0
+}
+
+Log_Purchase() { # synopsis: Log_Purchase Account_Name Purchase_Cost Purchase_Message # note: write a purchase entry to the log file. It needs the name of the account the purchase was made over, its cost and optionally a message to describe the purchase 
 		if [[ ${1+IsSet} != "IsSet" ]] ; then echo "Couldn't log purchase, missing argument: Account of the purchase. Status 1" > /dev/stderr ; return 1 ; fi
 		if [[ ${2+IsSet} != "IsSet" ]] ; then echo "Couldn't log purchase, missing argument: Cost of the purchase. Status 2" > /dev/stderr; return 2 ; fi
 		if [[ $# -gt 3 ]] ; then echo "Couldn't log purchase, excess of arguments. Status 3" > /dev/stderr ; return 3 ; fi
@@ -26,15 +35,17 @@ Log_Purchase() { # synopsis: Log_Purchase Account_Name Purchase_Cost Purchase_Me
 		if [[ ! "$Purchase_Cost" =~ ^[0-9]+$ ]] ; then echo "Couldn't log purchase, the Cost argument is not a (positive) integer. Status 5" > /dev/stderr ; return 5 ; fi
 		if ! File_Is_Account ~/moneybook/${Account_Name} ; then "Couldn't log purchase, the specified Account name couldn't be tracked to an Account File of the same name. Status 6" > /dev/stderr ; return 6 ; fi
 		
-		# Log file checks
-		if [[ ! -a "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_File}\` doesn't exist. Status 7" > /dev/stderr ; return 7 ; fi
-		if [[ ! -f "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_Path}\` is not a regular file. Status 8" > /dev/stderr ; return 8 ; fi
-		if [[ ! -w "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_File}\` is not writable. Status 9" > /dev/stderr ; return 9 ; fi
-		
+		# Log file check
+		Logging_Is_Possible # Should return 0
+		case $? in
+		1) echo "Couldn't log purchase, log file \`${Log_File_Path}\` doesn't exist. Status 7" > /dev/stderr ; return 7 ;;
+		2) echo "Couldn't log purchase, log file \`${Log_File_Path}\` is not a regular file. Status 8" > /dev/stderr ; return 8 ;;
+		3) echo "Couldn't log purchase, log file \`${Log_File_Path}\` is not writable. Status 9" > /dev/stderr ; return 9
+		esac
+
 		# Logging the purchase
 		Purchase_Log_Line=" [Purchase] ${Purchase_DateTime}, over the Account \`${Account_Name}\` with a cost of \`${Purchase_Cost}\`"
 		if [[ "$Purchase_Message" != "" ]] ; then Purchase_Log_Line+=": ${Purchase_Message}" ; fi
 		echo "$Purchase_Log_Line" >> "$Log_File_Path"
 		return 0
-
 }
