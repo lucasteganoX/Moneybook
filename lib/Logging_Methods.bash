8404f1d66bec14e4dad796c1c540efd889ba0f50
LucasYata <sgh4nl5forfocus@gmail.com>
2024-08-31 22:04:58 -0300

    Added library method to log a purchase
    Added. Not implemented into the program yet.

--- lib/Logging_Methods.bash
+++ lib/Logging_Methods.bash
@@ -1,1 +1,40 @@
 #!/bin/bash
+set -euo pipefail
+
+Log_Purchase() { # synopsis: Log_Purchase Account_Name Purchase_Cost Purchase_Message # note: write a purchase entry to the log file. It needs the name of the account the purchase was made over, its cost and optionally a message to describe the purchase 
+
+		if [[ ${1+IsSet} != "IsSet" ]] ; then echo "Couldn't log purchase, missing argument: Account of the purchase. Status 1" > /dev/stderr ; return 1 ; fi
+		if [[ ${2+IsSet} != "IsSet" ]] ; then echo "Couldn't log purchase, missing argument: Cost of the purchase. Status 2" > /dev/stderr; return 2 ; fi
+		if [[ $# -gt 3 ]] ; then echo "Couldn't log purchase, excess of arguments. Status 3" > /dev/stderr ; return 3 ; fi
+
+		declare -r Log_Directory_Path='/data/data/com.termux/files/usr/var/log'
+		declare -r Log_File_Path="${Log_Directory_Path}/moneybook.log"
+		declare -r Account_Name="$1"
+		declare -r +i Purchase_Cost="$2"
+		declare -r Purchase_Message="${3-}" # might be empty
+		declare -r Purchase_DateTime=$( date '+%a %b %e %Y %H:%Mhs' ) # The datetime might look like `Sat Aug 31 2024 16:20:57`
+		declare Purchase_Log_Line # The line that will be written to the log file
+
+		# Sourcing aka Imports
+		if ! . ~/moneybook/lib/Account_Methods.bash
+		then
+				echo "Logging_methods: Couldn't log purchase, couldn't source \`~/moneybook/lib/Account_Methods\`. Status 4"
+				return 4
+		fi
+
+		# Argument checks
+		if [[ ! "$Purchase_Cost" =~ ^[0-9]+$ ]] ; then echo "Couldn't log purchase, the Cost argument is not a (positive) integer. Status 5" > /dev/stderr ; return 5 ; fi
+		if ! File_Is_Account ~/moneybook/${Account_Name} ; then "Couldn't log purchase, the specified Account name couldn't be tracked to an Account File of the same name. Status 6" > /dev/stderr ; return 6 ; fi
+		
+		# Log file checks
+		if [[ ! -a "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_File}\` doesn't exist. Status 7" > /dev/stderr ; return 7 ; fi
+		if [[ ! -f "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_Path}\` is not a regular file. Status 8" > /dev/stderr ; return 8 ; fi
+		if [[ ! -w "$Log_File_Path" ]] ; then echo "Couldn't log purchase, log file \`${Log_File}\` is not writable. Status 9" > /dev/stderr ; return 9 ; fi
+		
+		# Logging the purchase
+		Purchase_Log_Line=" [Purchase] ${Purchase_DateTime}, over the Account \`${Account_Name}\` with a cost of \`${Purchase_Cost}\`"
+		if [[ "$Purchase_Message" != "" ]] ; then Purchase_Log_Line+=": ${Purchase_Message}" ; fi
+		echo "$Purchase_Log_Line" >> "$Log_File_Path"
+		return 0
+
+}
