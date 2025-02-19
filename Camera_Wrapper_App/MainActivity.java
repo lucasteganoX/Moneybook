f7d443f8b8575c343b454724aafa24863cb02727
LucasYata <sgh4nl5forfocus@gmail.com>
2025-02-19 01:55:56 -0300

    Initial commit
    Basically I have gathered a few basic notions of how to build a bare minimal android app and a few more things off of chatgpt and some android lessons. And thus is what I have got so far.
    I haven't really done much, I wanted to find a few stuff out(like remembering java for example) before getting the thing into git. But now it is the time so here we are :)

---
+++ Camera_Wrapper_App/MainActivity.java
@@ -0,0 +1,36 @@
+// package my.app
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate( Bundle bundle ) {
+			
+			Intent This_Intent = getIntent() ;
+			if ( This_Intent == null ) {
+					Log.e( "Main_Activity", "The intent was null somehow" ) ;
+					return
+			}
+
+			if ( ! This_Intent.hasExtra( MediaStore.EXTRA_OUTPUT ) ) {
+					Log.e( "Main_Activity", "The extra 'output' was not received." ) ;
+					return
+			}
+
+			// from https://developer.android.com/media/camera/camera-deprecated/photobasics?hl=en
+			if ( hasSystemFeature( PackageManager.FEATURE_CAMERA_ANY ) ) {
+					Log.e( "Main_Activity", "The phone doesn't have a camera available." ) ;
+					return
+			}
+
+			// get the output file
+			Uri OutputFile = This_Intent.getParcelableExtra( MediaStore.EXTRA_OUTPUT ) ;
+			
+			// Launch the IMAGE CAPTURE activity
+			
+			finish() ;
+	}
+}
