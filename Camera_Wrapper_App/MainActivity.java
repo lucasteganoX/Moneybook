81848dfd9a02a2e32fcce4e3063b00799b4634ee
LucasYata <sgh4nl5forfocus@gmail.com>
2025-02-20 15:35:41 -0300

    Check output file is a valid, writable uri

--- Camera_Wrapper_App/MainActivity.java
+++ Camera_Wrapper_App/MainActivity.java
@@ -6,6 +6,40 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+	private void Start_Camera( Intent Camera_Intent ) {
+			// According to chatgpt, if your android version(reflected in the sdk version) is older than x
+			// then you need yo use x method and otherwise use y. So I will do each according to that.
+			const int New_Method_API_Version = Build.VERSION_CODES.30 ;
+			const int Current_API_Version = Build.VERSION.SDK_INT ;
+			
+			if ( Current_API_Version >= New_Method_API_Version ) {
+					
+			}
+			else {
+
+			}
+	}
+
+	private bool Uri_Is_Valid( Uri File_Path ) {
+			if ( OutputFile == null || OutputFile.toString().isEmpty() ) {
+		    		Log.e("MainActivity", "The output file is either null or empty.");    
+				    return false ;
+			}
+
+			String OutputFile_Scheme = outputUri.getScheme();
+			if ( OutputFile_Scheme == null || ( ! OutputFile_Scheme.equals( "file" ) && ! OutputFile_Scheme.equals( "content" ) ) ) {
+				    Log.e("MainActivity", "Expected file or content scheme, but received: `" + OutputFile_Scheme + "` instead." );
+					return false ;
+			}
+
+			return true ;
+	}
+
+	private bool Uri_Is_Writable( Uri File_Path ) {
+			try { OutputStream Stream = getContentResolver().openOutputStream( File_Path ) ; return true ; }
+			catch { return false ; }
+	}
+
 	@Override
 	protected void onCreate( Bundle bundle ) {
 			
@@ -19,6 +53,19 @@ public class MainActivity extends Activity {
 					Log.e( "Main_Activity", "The extra 'output' was not received." ) ;
 					return
 			}
+			
+			// get the output file
+			Uri OutputFile = This_Intent.getParcelableExtra( MediaStore.EXTRA_OUTPUT ) ;
+
+			if ( ! Uri_Is_Valid( OutputFile ) ) {
+					Log.e( "Main_Activity", "Couldn't parse the output file as a valid URI." ) ;
+					return ;
+			}
+
+			if ( ! Uri_Is_Writable( OutputFile ) ) {
+					Log.e( "Main_Activity", "The output file was identified as unwritable." ) ;
+					return ;
+			}
 
 			// from https://developer.android.com/media/camera/camera-deprecated/photobasics?hl=en
 			if ( hasSystemFeature( PackageManager.FEATURE_CAMERA_ANY ) ) {
@@ -26,13 +73,20 @@ public class MainActivity extends Activity {
 					return
 			}
 
-			// get the output file
-			Uri OutputFile = This_Intent.getParcelableExtra( MediaStore.EXTRA_OUTPUT ) ;
-			
 			// Launch the IMAGE CAPTURE activity
 			Intent IMAGE_CAPTURE_Intent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE ) ;
-			startActivity(intent) ;
+			IMAGE_CAPTURE_Intent.putExtra( MediaStore.EXTRA_OUTPUT, OutputFile ) ;
 
+			const bool Camera_App_Available = ( IMAGE_CAPTURE_Intent.resolveActivity( getPackageManager() ) != null ) ;
+			if ! ( Camera_App_Available ) {
+					Log.e( "Main_Activity", "There's no camera app available to capture the photo.") ;
+					return ;
+			}
+
+			Start_Camera( IMAGE_CAPTURE_Intent ) ;
+
+			startActivity(intent) ;
+			
 			finish() ;
 	}
 }
