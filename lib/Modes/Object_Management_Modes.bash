dd5b25e8ff703b4f0bfc577539acc685a607f8d4
LucasYata <sgh4nl5forfocus@gmail.com>
2025-10-28 13:03:32 -0300

    Add the method signature for function that creates Account objects
    This is the function that creates them, not the creation mode flow of the program.

---
+++ lib/Modes/Object_Management_Modes.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+Create_Account() {
+# Synopsis: Create_Account <String>Account_Name <+Int>Account_Share <Int>Account_Funds [<String?>File_Message]
+# Note: The account funds might be negative, positive or zero.
+# Note: The file message is a text appended to the account object file to be created. It is not expanded.
+
+}
